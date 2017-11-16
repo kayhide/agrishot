8fd55c022e067774322ce7d64ba7fd5a93408269
kayhide <kayhide@gmail.com>
2017-11-16 21:16:07 +0900

    update backend lib/localstack for credentials.

--- backend/lib/localstack.js
+++ backend/lib/localstack.js
@@ -2,11 +2,18 @@
 
 const AWS = require('aws-sdk');
 
-function localize(base, origin, extras = {}) {
+function localize(base, origin) {
   return class {
     constructor(options = {}) {
-      Object.assign(options, { region: 'us-east-1', endpoint: `http://${origin}` });
-      Object.assign(options, extras);
+      Object.assign(options, {
+        region: 'us-east-1',
+        endpoint: `http://${origin}`,
+        credentials: new AWS.Credentials({
+          accessKeyId: '',
+          secretAccessKey: ''
+        }),
+        s3ForcePathStyle: true
+      });
       return new base(options);
     }
   };
@@ -14,5 +21,5 @@ function localize(base, origin, extras = {}) {
 
 module.exports.DynamoDB = localize(AWS.DynamoDB, 'localhost:4569');
 module.exports.DynamoDB.DocumentClient = localize(AWS.DynamoDB.DocumentClient, 'localhost:4569');
-module.exports.S3 = localize(AWS.S3, 'localhost:4572', { s3ForcePathStyle: true });
+module.exports.S3 = localize(AWS.S3, 'localhost:4572');
 module.exports.CloudFormation = localize(AWS.CloudFormation, 'localhost:4581');
