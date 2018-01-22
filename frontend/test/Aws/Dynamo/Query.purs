ac992e236357d883b61642efb46ca898ab46b403
kayhide <kayhide@gmail.com>
2018-01-22 21:30:32 +0900

    update tests for Aws.Dynamo.Query.

---
+++ frontend/test/Aws/Dynamo/Query.purs
@@ -0,0 +1,143 @@
+module Test.Aws.Dynamo.Query where
+
+import Prelude
+
+import Aws.Dynamo (DYNAMO)
+import Aws.Dynamo.Query as DQ
+import Control.Monad.Aff (Aff)
+import Control.Monad.Aff.Console (CONSOLE)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Random (RANDOM)
+import Data.Array as Array
+import Data.Foldable (and)
+import Data.Foreign.Class (encode)
+import Test.QuickCheck (Result, quickCheck, (===))
+import Unsafe.Coerce (unsafeCoerce)
+
+
+type Aff_ eff a = Aff
+                  ( console :: CONSOLE
+                  , random :: RANDOM
+                  , exception :: EXCEPTION
+                  , dynamo :: DYNAMO
+                  | eff) a
+
+test :: forall eff. Aff_ eff Unit
+test = do
+  testParams
+  testExpression
+
+
+testParams :: forall eff. Aff_ eff Unit
+testParams = liftEff $ do
+  quickCheck checkTableName
+  quickCheck checkIndexName
+  quickCheck checkAscending
+  quickCheck checkDescending
+  quickCheck checkLimit
+  quickCheck checkKeyCondition
+  quickCheck checkFilter
+
+checkTableName :: String -> Result
+checkTableName name = params."TableName" === name
+  where
+    params = unsafeCoerce $ encode do
+      DQ.tableName name
+
+checkIndexName :: String -> Result
+checkIndexName name = params."IndexName" === name
+  where
+    params = unsafeCoerce $ encode do
+      DQ.indexName name
+
+checkAscending :: Unit -> Result
+checkAscending _ = params."ScanIndexForward" === true
+  where
+    params = unsafeCoerce $ encode do
+      DQ.ascending
+
+checkDescending :: Unit -> Result
+checkDescending _ = params."ScanIndexForward" === false
+  where
+    params = unsafeCoerce $ encode do
+      DQ.descending
+
+checkLimit :: Int -> Result
+checkLimit i = params."Limit" === i
+  where
+    params = unsafeCoerce $ encode do
+      DQ.limit i
+
+checkKeyCondition :: String -> Number -> Boolean
+checkKeyCondition s n =
+  and
+  [ params."ExpressionAttributeNames"."#name" == "name"
+  , params."ExpressionAttributeNames"."#age" == "age"
+  , params."ExpressionAttributeValues".":1" == s
+  , params."ExpressionAttributeValues".":2" == n
+  , params."KeyConditionExpression" == "(#name = :1) AND (#age < :2)"
+  ]
+  where
+    params = unsafeCoerce $ encode do
+      DQ.keyCondition $
+        DQ.and_
+        [ DQ.eq_ "#name" s
+        , DQ.lt_ "#age" n
+        ]
+
+checkFilter :: String -> Number -> Boolean
+checkFilter s n =
+  and
+  [ params."ExpressionAttributeNames"."#name" == "name"
+  , params."ExpressionAttributeNames"."#age" == "age"
+  , params."ExpressionAttributeValues".":1" == s
+  , params."ExpressionAttributeValues".":2" == n
+  , params."FilterExpression" == "(#name = :1) AND (#age < :2)"
+  ]
+  where
+    params = unsafeCoerce $ encode do
+      DQ.filter $
+        DQ.and_
+        [ DQ.eq_ "#name" s
+        , DQ.lt_ "#age" n
+        ]
+
+testExpression :: forall eff. Aff_ eff Unit
+testExpression = liftEff do
+  quickCheck \s -> check "#name = :1" (DQ.eq_ "#name" (s :: String))
+  quickCheck \s -> check "#name <> :1" (DQ.ne_ "#name" (s :: String))
+  quickCheck \i -> check "#age < :1" (DQ.lt_ "#age" (i :: Int))
+  quickCheck \i -> check "#age <= :1" (DQ.le_ "#age" (i :: Int))
+  quickCheck \i -> check "#age > :1" (DQ.gt_ "#age" (i :: Int))
+  quickCheck \i -> check "#age >= :1" (DQ.ge_ "#age" (i :: Int))
+  quickCheck \i j -> check "#age BETWEEN :1 AND :2" (DQ.between_ "#age" (i :: Int) (j :: Int))
+  quickCheck \is ->
+    check ("#age IN ("
+           <> (Array.intercalate "," $
+               (":" <> _) <<< show <<< (_ + 1) <$> Array.mapWithIndex const is
+              )
+           <> ")") $
+    DQ.in_ "#age" (is :: Array Int)
+
+  quickCheck \x y ->
+    check "(#name = :1) AND (#age < :2)" $
+    DQ.and_
+    [ DQ.eq_ "#name" (x :: String)
+    , DQ.lt_ "#age" (y :: Int)
+    ]
+
+  quickCheck \x y ->
+    check "(#name = :1) OR (#age < :2)" $
+    DQ.or_
+    [ DQ.eq_ "#name" (x :: String)
+    , DQ.lt_ "#age" (y :: Int)
+    ]
+
+  quickCheck \x ->
+    check "NOT #name = :1" $
+    DQ.not_ $
+    DQ.eq_ "#name" (x :: String)
+
+  where
+    check str exp = str === (_."FilterExpression" <<< unsafeCoerce <<< encode $ DQ.filter exp)
--- frontend/test/Main.purs
+++ frontend/test/Main.purs
@@ -3,26 +3,20 @@ module Test.Main where
 import Prelude
 
 import Aws.Config as AwsConfig
-import Aws.Dynamo (DYNAMO, ScanResult)
+import Aws.Dynamo (DYNAMO)
 import Aws.Dynamo as Dynamo
-import Aws.Dynamo.Query as DQ
 import Control.Monad.Aff (Aff, runAff_)
 import Control.Monad.Aff.Console (log, logShow)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, errorShow)
-import Control.Monad.Eff.Exception (EXCEPTION, error, try)
+import Control.Monad.Eff.Exception (EXCEPTION, try)
 import Control.Monad.Eff.Random (RANDOM)
-import Control.Monad.Except (runExcept, throwError)
 import Data.Either (Either(..))
-import Data.Foreign (Foreign)
-import Data.Foreign.Class (decode)
-import Data.List.NonEmpty as NEL
-import Data.Traversable (traverse, traverse_)
 import Dom.Meta (META)
 import Dom.Meta as Meta
-import Model.Photo (Photo)
 import Test.Aws.Dynamo as AwsDynamo
+import Test.Aws.Dynamo.Query as AwsDynamoQuery
 
 
 type AppEffs = ( console :: CONSOLE
@@ -47,49 +41,9 @@ main :: Eff AppEffs Unit
 main = runAff_ errorShow do
   liftEff setup
   AwsDynamo.test
-  testDynamo
-  testDynamoQuery
+  AwsDynamoQuery.test
   testMeta
 
-testDynamo :: Aff AppEffs Unit
-testDynamo = do
-  res <- Dynamo.scan opts
-  logShow res."Count"
-  photos <- getItems (res :: ScanResult Foreign)."Items"
-  traverse_ logShow photos
-
-  where
-    opts = { "TableName": "agrishot-test-photos" }
-
-getItems :: forall eff_. Array Foreign -> Aff eff_ (Array Photo)
-getItems objs =
-  case runExcept (traverse decode objs) of
-    Right xs -> pure xs
-    Left err -> throwError $ error $ show $ NEL.head err
-
-testDynamoQuery :: Aff AppEffs Unit
-testDynamoQuery = do
-  res <- Dynamo.query q
-  photos <- getItems res."Items"
-  traverse_ logShow photos
-  pure unit
-  where
-    q = do
-      DQ.tableName "agrishot-test-photos"
-      DQ.indexName "agrishot-test-photos-part-created_at"
-      DQ.ascending
-      DQ.limit 5
-      DQ.keyCondition $
-        DQ.and_
-        [ DQ.eq_ "#part" 0
-        , DQ.between_ "#created_at" 1511354013941.0 1511358491331.0
-        ]
-      DQ.filter $
-        DQ.and_
-        [ DQ.ne_ "#sender_id" "#image_url"
-        , DQ.in_ "#sender_id" ["line:U1278409", "hoge"]
-        , DQ.in_ "#sender.#provider" ["line", "hoge"]
-        ]
 
 testMeta :: Aff AppEffs Unit
 testMeta = do
